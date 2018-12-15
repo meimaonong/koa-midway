bbfd22020c1382bb00f503ff8915a5fa3a93f685
覃超成 <meimaonong@163.com>
2018-12-15 11:59:32 +0800

    调整

--- src/mid-pc/app.js
+++ src/mid-pc/app.js
@@ -1,6 +1,8 @@
 const Koa = require("koa");
+const Router = require("koa-router");
 
 const app = new Koa();
+const router = new Router();
 
 router.get("/", (ctx, next) => {
   ctx.body = "midway";
